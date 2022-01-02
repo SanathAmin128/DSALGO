aff72b7a3278065b14b2994f02b62681cf59331e
Sanath Amin <sanathamin128@gmail.com>
2022-01-02 19:33:06 +0530

    new commit

---
+++ src/main/java/Stack_6/online_stock_span_2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Stack_6;
+
+/**
+ *
+ * @author SANATH
+ */
+public class online_stock_span_2
+{
+     Stack<int[]> s;
+    public StockSpanner() {
+        s=new Stack<>();
+    }
+    
+    public int next(int price) {
+        int ans=1;
+        while(!s.empty() && s.peek()[0]<=price)
+              ans+=(s.pop()[1]);
+        s.push(new int[]{price, ans});
+        return ans;
+                
+        
+}
