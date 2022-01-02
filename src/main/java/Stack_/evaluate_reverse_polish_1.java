e45db16dbbd74f8acf25ae8f2b577fef9503c1af
Sanath Amin <sanathamin128@gmail.com>
2022-01-02 12:05:12 +0530

    new commit

---
+++ src/main/java/Stack_/evaluate_reverse_polish_1.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Stack_;
+
+/**
+ *
+ * @author SANATH
+ */
+public class evaluate_reverse_polish_1
+{
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> sta=new Stack<>();
+        int res;
+        for(int i=0;i<tokens.length;i++)
+        { 
+            String s="+-*/";
+            if(!s.contains(tokens[i]))
+                sta.push(Integer.valueOf(tokens[i]));
+            else
+            {
+                int num2=sta.pop();
+                int num1=sta.pop();
+                res=calculate(num1,num2,tokens[i]);
+                sta.push(res);
+            }
+        }
+        return sta.pop();
+    }
+    public int calculate(int num1,int num2,String opp)
+    {
+        int res=0;
+        switch(opp)
+        {
+           
+            case "+":res=num1+num2;
+                     break;
+            case "-":res=num1-num2;
+                     break;
+            case "*":res=num1*num2;
+                     break;
+            case "/":res=num1/num2;
+                     break;
+        }
+        return res;
+}
